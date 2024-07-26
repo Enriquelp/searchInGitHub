8c7d9984b34b2e2bfffaa96d4630b9ccde00adc2
Enriquelp <enriquelopezencinas@uma.es>
2024-07-26 10:41:16 +0200

    Arreglo de sobreescritura de archivos con el mismo nombre pero rutas diferentes

--- searchYAML.py
+++ searchYAML.py
@@ -12,6 +12,15 @@ def buscar_archivos_yaml(directorio_raiz):
                 archivos_yaml.append(os.path.join(root, file))
     return archivos_yaml
 
+def generar_nombre_unico(dest, nombre_archivo):
+    nombre, extension = os.path.splitext(nombre_archivo)
+    contador = 1
+    nuevo_nombre = nombre_archivo
+    while os.path.exists(os.path.join(dest, nuevo_nombre)):
+        nuevo_nombre = f"{nombre}{contador}{extension}"
+        contador += 1
+    return nuevo_nombre
+
 def main(root, dest):
     print(f'Buscando archivos yaml...')
     directorio_raiz = root  
@@ -21,8 +30,13 @@ def main(root, dest):
 
     # copiar los archivos .yaml encontrados
     for archivo in archivos_yaml:
-        shutil.copy2(archivo, dest)
+        nombre_archivo = os.path.basename(archivo)
+        nombre_unico = generar_nombre_unico(dest, nombre_archivo)
+        shutil.copy2(archivo, os.path.join(dest, nombre_unico))
     print(f"se han copiado {len(archivos_yaml)} archivos")
     return len(archivos_yaml)
-    
+
+# Llamada de prueba a la función main
+# main('/ruta/de/origen', '/ruta/de/destino')
+
 
